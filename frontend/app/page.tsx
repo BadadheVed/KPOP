db5a67f2efdf05ba17a6f57e636c65b45e6a9e23
Ved Badadhe <vbbadadhe@gmail.com>
2025-09-09 04:17:19 +0530

    Scrolling issue resolved

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Img1 from "@/components/img1";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen m-0 p-0" style={{ margin: 0, padding: 0 }}>
       <Img1 />
     </div>
   );
--- frontend/components/img1.tsx
+++ frontend/components/img1.tsx
@@ -2,252 +2,259 @@ export default function Img1() {
   return (
     <div
       style={{
-        width: "100%",
-        height: "100%",
+        width: "100vw",
+        minHeight: "100vh",
         position: "relative",
-        background: "white",
-        overflow: "hidden",
+        background: "#270157",
+        overflow: "auto", // Enable scrolling
+        margin: 0,
+        padding: 0,
       }}
     >
-      <img
-        style={{
-          width: 1440,
-          height: 1024,
-          left: 2,
-          top: 0,
-          position: "absolute",
-        }}
-        src="./lift-l.png"
-      />
-      <img
-        style={{
-          width: 1440,
-          height: 1024,
-          left: 2,
-          top: 0,
-          position: "absolute",
-        }}
-        src="./lift-r.png"
-      />
-      <img
-        style={{
-          width: 1440,
-          height: 1024,
-          left: 0,
-          top: 0,
-          position: "absolute",
-        }}
-        src="./door-l.png"
-      />
-      <img
-        style={{
-          width: 1440,
-          height: 1024,
-          left: 0,
-          top: 0,
-          position: "absolute",
-        }}
-        src="./door-r.png"
-      />
-      <img
-        style={{
-          width: 1440,
-          height: 1024,
-          left: 0,
-          top: 0,
-          position: "absolute",
-        }}
-        src="./interior-hanguk.png"
-      />
-      <img
-        style={{
-          width: 1440,
-          height: 1024,
-          left: 1434,
-          top: 1024,
-          position: "absolute",
-        }}
-        src="./lift-l.png"
-      />
-      <img
-        style={{
-          width: 1440,
-          height: 1024,
-          left: 0,
-          top: 0,
-          position: "absolute",
-        }}
-        src="./ext-hangukverse.png"
-      />
-      {/* <img
-        style={{
-          width: 1440,
-          height: 318,
-          left: 2,
-          top: 0,
-          position: "absolute",
-        }}
-        src="https://placehold.co/1440x318"
-      /> */}
+      {/* Container for all images with fixed dimensions */}
       <div
-        data-property-1="Group 1"
         style={{
-          width: 179,
-          height: 38,
-          left: 828,
-          top: 33,
-          position: "absolute",
+          width: "100%",
+          minWidth: "1440px", // Ensure minimum width to show full image
+          height: "1024px", // Fixed height to maintain aspect ratio
+          position: "relative",
+          margin: "0 auto", // Center the content
         }}
       >
+        {/* Background layers - maintaining original dimensions */}
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            objectFit: "contain", // Show entire image without cropping
+          }}
+          src="./lift-l.png"
+          alt="Lift left"
+        />
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            objectFit: "contain",
+          }}
+          src="./lift-r.png"
+          alt="Lift right"
+        />
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            objectFit: "contain",
+          }}
+          src="./door-l.png"
+          alt="Door left"
+        />
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            objectFit: "contain",
+          }}
+          src="./door-r.png"
+          alt="Door right"
+        />
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            objectFit: "contain",
+          }}
+          src="./interior-hanguk.png"
+          alt="Interior"
+        />
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            objectFit: "contain",
+          }}
+          src="./ext-hangukverse.png"
+          alt="Exterior hangukverse"
+        />
+
+        {/* Navigation buttons - positioned according to original design */}
         <div
           style={{
-            width: 179,
-            height: 38,
-            left: 0,
-            top: 0,
             position: "absolute",
-            opacity: 0.66,
-            background: "#630063",
-            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            borderRadius: 30,
+            top: "33px", // Using original pixel values
+            right: "32px",
+            display: "flex",
+            gap: "20px",
+            zIndex: 10,
           }}
-        />
-      </div>
-      <div
-        data-property-1="Group 1"
-        style={{
-          width: 179,
-          height: 38,
-          left: 1029,
-          top: 33,
-          position: "absolute",
-        }}
-      >
-        <div
-          style={{
-            width: 179,
-            height: 38,
-            left: 0,
-            top: 0,
-            position: "absolute",
-            opacity: 0.66,
-            background: "#630063",
-            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            borderRadius: 30,
-          }}
-        />
-      </div>
-      <div
-        data-property-1="Group 1"
-        style={{
-          width: 179,
-          height: 38,
-          left: 1220,
-          top: 33,
-          position: "absolute",
-        }}
-      >
-        <div
-          style={{
-            width: 179,
-            height: 38,
-            left: 0,
-            top: 0,
-            position: "absolute",
-            opacity: 0.66,
-            background: "#630063",
-            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            borderRadius: 30,
-          }}
-        />
-      </div>
-      <div
-        style={{
-          width: 111,
-          height: 25,
-          left: 862,
-          top: 39,
-          position: "absolute",
-          color: "white",
-          fontSize: 16,
-          fontFamily: "Kalnia",
-          fontWeight: "300",
-          letterSpacing: 0.96,
-          wordWrap: "break-word",
-        }}
-      >
-        Community
-      </div>
-      <div
-        style={{
-          width: 111,
-          height: 25,
-          left: 1066,
-          top: 39,
-          position: "absolute",
-          color: "white",
-          fontSize: 16,
-          fontFamily: "Kalnia",
-          fontWeight: "300",
-          letterSpacing: 0.96,
-          wordWrap: "break-word",
-        }}
-      >
-        Pro Member
-      </div>
-      <div
-        style={{
-          width: 134,
-          height: 25,
-          left: 1248,
-          top: 39,
-          position: "absolute",
-          color: "white",
-          fontSize: 16,
-          fontFamily: "Kalnia",
-          fontWeight: "300",
-          letterSpacing: 0.96,
-          wordWrap: "break-word",
-        }}
-      >
-        login / sign up
-      </div>
-      <div
-        data-property-1="Group 1"
-        style={{
-          width: 157,
-          height: 24,
-          left: 643,
-          top: 664,
-          position: "absolute",
-        }}
-      >
+        >
+          {/* Community Button */}
+          <div
+            style={{
+              width: 179,
+              height: 38,
+              position: "relative",
+            }}
+          >
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                opacity: 0.66,
+                background: "#630063",
+                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                borderRadius: 30,
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                color: "white",
+                fontSize: 16,
+                fontFamily: "Kalnia, sans-serif",
+                fontWeight: "300",
+                letterSpacing: 0.96,
+                whiteSpace: "nowrap",
+              }}
+            >
+              Community
+            </div>
+          </div>
+
+          {/* Pro Member Button */}
+          <div
+            style={{
+              width: 179,
+              height: 38,
+              position: "relative",
+            }}
+          >
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                opacity: 0.66,
+                background: "#630063",
+                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                borderRadius: 30,
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                color: "white",
+                fontSize: 16,
+                fontFamily: "Kalnia, sans-serif",
+                fontWeight: "300",
+                letterSpacing: 0.96,
+                whiteSpace: "nowrap",
+              }}
+            >
+              Pro Member
+            </div>
+          </div>
+
+          {/* Login/Sign up Button */}
+          <div
+            style={{
+              width: 179,
+              height: 38,
+              position: "relative",
+            }}
+          >
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                opacity: 0.66,
+                background: "#630063",
+                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                borderRadius: 30,
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                color: "white",
+                fontSize: 16,
+                fontFamily: "Kalnia, sans-serif",
+                fontWeight: "300",
+                letterSpacing: 0.96,
+                whiteSpace: "nowrap",
+              }}
+            >
+              login / sign up
+            </div>
+          </div>
+        </div>
+
+        {/* Click Here button - positioned according to original design */}
         <div
           style={{
             width: 157,
             height: 24,
-            left: 0,
-            top: 0,
             position: "absolute",
-            opacity: 0.66,
-            background: "#270157",
-            borderRadius: 30,
-          }}
-        />
-        <div
-          style={{
-            left: 37,
-            top: 2,
-            position: "absolute",
-            color: "white",
-            fontSize: 14,
-            fontFamily: "Kalnia",
-            fontWeight: "300",
-            letterSpacing: 0.84,
-            wordWrap: "break-word",
+            top: "664px", // Using original pixel value
+            left: "50%",
+            transform: "translateX(-50%)",
+            zIndex: 10,
           }}
         >
-          Click Here
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              position: "absolute",
+              opacity: 0.66,
+              background: "#270157",
+              borderRadius: 30,
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              color: "white",
+              fontSize: 14,
+              fontFamily: "Kalnia, sans-serif",
+              fontWeight: "300",
+              letterSpacing: 0.84,
+            }}
+          >
+            Click Here
+          </div>
         </div>
       </div>
     </div>
