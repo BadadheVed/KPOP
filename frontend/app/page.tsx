39a3cc5cc823ed54865fee6338d5f0ac7ee4405e
Ved Badadhe <vbbadadhe@gmail.com>
2025-09-09 03:54:24 +0530

    Initial Commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Img1 from "@/components/img1";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen">
+      <Img1 />
+    </div>
+  );
+}
---
+++ frontend/components/img1.tsx
@@ -0,0 +1,255 @@
+export default function Img1() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        background: "white",
+        overflow: "hidden",
+      }}
+    >
+      <img
+        style={{
+          width: 1440,
+          height: 1024,
+          left: 2,
+          top: 0,
+          position: "absolute",
+        }}
+        src="./lift-l.png"
+      />
+      <img
+        style={{
+          width: 1440,
+          height: 1024,
+          left: 2,
+          top: 0,
+          position: "absolute",
+        }}
+        src="./lift-r.png"
+      />
+      <img
+        style={{
+          width: 1440,
+          height: 1024,
+          left: 0,
+          top: 0,
+          position: "absolute",
+        }}
+        src="./door-l.png"
+      />
+      <img
+        style={{
+          width: 1440,
+          height: 1024,
+          left: 0,
+          top: 0,
+          position: "absolute",
+        }}
+        src="./door-r.png"
+      />
+      <img
+        style={{
+          width: 1440,
+          height: 1024,
+          left: 0,
+          top: 0,
+          position: "absolute",
+        }}
+        src="./interior-hanguk.png"
+      />
+      <img
+        style={{
+          width: 1440,
+          height: 1024,
+          left: 1434,
+          top: 1024,
+          position: "absolute",
+        }}
+        src="./lift-l.png"
+      />
+      <img
+        style={{
+          width: 1440,
+          height: 1024,
+          left: 0,
+          top: 0,
+          position: "absolute",
+        }}
+        src="./ext-hangukverse.png"
+      />
+      {/* <img
+        style={{
+          width: 1440,
+          height: 318,
+          left: 2,
+          top: 0,
+          position: "absolute",
+        }}
+        src="https://placehold.co/1440x318"
+      /> */}
+      <div
+        data-property-1="Group 1"
+        style={{
+          width: 179,
+          height: 38,
+          left: 828,
+          top: 33,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 179,
+            height: 38,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            opacity: 0.66,
+            background: "#630063",
+            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            borderRadius: 30,
+          }}
+        />
+      </div>
+      <div
+        data-property-1="Group 1"
+        style={{
+          width: 179,
+          height: 38,
+          left: 1029,
+          top: 33,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 179,
+            height: 38,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            opacity: 0.66,
+            background: "#630063",
+            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            borderRadius: 30,
+          }}
+        />
+      </div>
+      <div
+        data-property-1="Group 1"
+        style={{
+          width: 179,
+          height: 38,
+          left: 1220,
+          top: 33,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 179,
+            height: 38,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            opacity: 0.66,
+            background: "#630063",
+            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            borderRadius: 30,
+          }}
+        />
+      </div>
+      <div
+        style={{
+          width: 111,
+          height: 25,
+          left: 862,
+          top: 39,
+          position: "absolute",
+          color: "white",
+          fontSize: 16,
+          fontFamily: "Kalnia",
+          fontWeight: "300",
+          letterSpacing: 0.96,
+          wordWrap: "break-word",
+        }}
+      >
+        Community
+      </div>
+      <div
+        style={{
+          width: 111,
+          height: 25,
+          left: 1066,
+          top: 39,
+          position: "absolute",
+          color: "white",
+          fontSize: 16,
+          fontFamily: "Kalnia",
+          fontWeight: "300",
+          letterSpacing: 0.96,
+          wordWrap: "break-word",
+        }}
+      >
+        Pro Member
+      </div>
+      <div
+        style={{
+          width: 134,
+          height: 25,
+          left: 1248,
+          top: 39,
+          position: "absolute",
+          color: "white",
+          fontSize: 16,
+          fontFamily: "Kalnia",
+          fontWeight: "300",
+          letterSpacing: 0.96,
+          wordWrap: "break-word",
+        }}
+      >
+        login / sign up
+      </div>
+      <div
+        data-property-1="Group 1"
+        style={{
+          width: 157,
+          height: 24,
+          left: 643,
+          top: 664,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 157,
+            height: 24,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            opacity: 0.66,
+            background: "#270157",
+            borderRadius: 30,
+          }}
+        />
+        <div
+          style={{
+            left: 37,
+            top: 2,
+            position: "absolute",
+            color: "white",
+            fontSize: 14,
+            fontFamily: "Kalnia",
+            fontWeight: "300",
+            letterSpacing: 0.84,
+            wordWrap: "break-word",
+          }}
+        >
+          Click Here
+        </div>
+      </div>
+    </div>
+  );
+}
